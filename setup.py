d8bb441eee45bf0a0643aab632b6f0af3b79e99b
Olawoye Taofeek <habephe21@gmail.com>
2024-09-06 11:39:58 +0100

    First Commit

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_packages(file_path: str) -> List[str]:
+    """
+    Retrieve all package dependencies from a requirements.txt file.
+
+    Args:
+        file_path (str): Path to the requirements.txt file.
+
+    Returns:
+        List[str]: A list of package names without version specifiers or "-e .".
+    """
+    with open(file_path, "r") as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and not req.startswith("-e .")]
+        return requirements
 
 setup(
-    name="Oladipupo Ml Project",
-    version= '0.0.1',
+    name="Oladipupo ML Project",
+    version="0.0.1",
     author="Taofeek Opeyemi Olawoye",
-    author_email= "habephe21@gmail.com",
+    author_email="habephe21@gmail.com",
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn', 'scikit-learn', 
-                      'matplotlib', 'seaborn', 'pillow'],
+    install_requires=get_packages('requirements.txt')
 )
