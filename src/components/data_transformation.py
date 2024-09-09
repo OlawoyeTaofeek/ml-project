00b0e2b1f858ffbfef18452d358a8def21eb731f
Olawoye Taofeek <habephe21@gmail.com>
2024-09-09 20:47:13 +0100

    Data Transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,109 @@
+import numpy as np
+import sys
+from dataclasses import dataclass
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exceptions import CustomException
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessing_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+        
+    def get_data_transformer_object(self):
+        try:
+            pass
+        except:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
