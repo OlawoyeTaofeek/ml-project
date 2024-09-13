4e68c53ba7ce2391d619bc362216dd91e16ba2a5
Olawoye Taofeek <habephe21@gmail.com>
2024-09-13 15:48:24 +0100

    Creating Pickle file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -25,12 +29,12 @@ class DataIngestion:
             logging.info("Dataset read correctly into DataFrame")
             os.makedirs(os.path.dirname(self.Ingestion_config.TRAIN_DATA_PATH), exist_ok=True)
             os.makedirs(os.path.dirname(self.Ingestion_config.TEST_DATA_PATH), exist_ok=True)
-            df.to_csv(self.Ingestion_config.RAW_DATA_PATH, index=False, header=False)
+            df.to_csv(self.Ingestion_config.RAW_DATA_PATH, index=False, header=True)
             
             logging.info("Train test split Initiating!!")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            train_set.to_csv(self.Ingestion_config.TRAIN_DATA_PATH, index=False, header=False)
-            test_set.to_csv(self.Ingestion_config.TEST_DATA_PATH, index=False, header=False)
+            train_set.to_csv(self.Ingestion_config.TRAIN_DATA_PATH, index=False, header=True)
+            test_set.to_csv(self.Ingestion_config.TEST_DATA_PATH, index=False, header=True)
             
             logging.info("Ingestion of the data is completed")
             
@@ -43,9 +47,12 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 
-if __name__== "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 
